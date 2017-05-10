ec087539f0da73848a9e533e3b39e708fe513d03
Heritiana Rabary <marvduck7@gmail.com>
2017-05-10 16:22:28 +0200

    review.rb updated

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, presence: true
+  # TODO: add validation that check if rating is an integer
   validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5], allow_nil: false }
 end
